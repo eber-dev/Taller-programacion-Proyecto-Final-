803330182602f7f6ac82034f47e6fe3d82eff0b9
Eber <www.eberacunaquispe@gmail.com>
2025-10-08 00:04:38 -0500

    Avance de las funciones de cuenta por año, mes, mes por temperatura y hora

---
+++ Proyecto_Final/src/datos/ProcesarDatos.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+import model.clima;
+import utileria.metodos;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author EBER
+ */
+public class ProcesarDatos {
+    
+    public static Map<Integer, Integer> Cuenta_Año(List<clima> datos, int inicio, int fin){
+        Map<Integer, Integer> organizar = new TreeMap<>();
+        for(clima recorre: datos){
+            int año_formato = metodos.obtener_año(recorre.getfecha());
+            if(año_formato>=inicio && año_formato<= fin){
+                organizar.put(año_formato, organizar.getOrDefault(año_formato,0) + 1);   
+            }
+        }
+        
+        return organizar;   
+    }
+    
+    public static Map<Integer,Integer> Cuenta_Mes_Año(List<clima> datos, int year) {
+        Map<Integer,Integer> organizar = new TreeMap<>();
+        for (clima c : datos) {
+            int año_formato = metodos.obtener_año(c.getfecha());
+            if (año_formato == year) {
+                int mes_formato = metodos.obtener_mes(c.getfecha());
+                organizar.put(mes_formato, organizar.getOrDefault(mes_formato,0) + 1);
+            }
+        }
+        return organizar;
+    }
+    
+    
+    public static Map<Integer,Integer> Cuenta_Mes_Temp(List<clima> datos, int year, double minTemp, double maxTemp) {
+        Map<Integer,Integer> organizar = new TreeMap<>();
+        for (clima c : datos) {
+            int año_formato = metodos.obtener_año(c.getfecha());
+            if (año_formato == year && c.gettemperatura() >= minTemp && c.gettemperatura() <= maxTemp) {
+                int mes_formato = metodos.obtener_mes(c.getfecha());
+                organizar.put(mes_formato, organizar.getOrDefault(mes_formato,0) + 1);
+            }
+        }
+        return organizar;
+    }
+    
+    
+    public static Map<Integer,Integer> Cuenta_Hora(List<clima> datos, int year) {
+        Map<Integer,Integer> organizar = new TreeMap<>();
+        for (clima c : datos) {
+            int año_formato = metodos.obtener_año(c.getfecha());
+            if (año_formato == year) {
+                int hora_formato = metodos.obtener_hora(c.gethora());
+                organizar.put(hora_formato, organizar.getOrDefault(hora_formato,0) + 1);
+            }
+        }
+        return organizar;
+    }
+        
+}
